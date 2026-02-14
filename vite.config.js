02a4fa20fdf3db77a4f5384fba14b13ca0f24350
rave-roses <odekunlewaris01@gmail.com>
2026-02-14 15:02:28 +0100

    input base to load all file on vercel

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import Components from "unplugin-vue-components/vite";
 import MotionResolver from "motion-v/resolver";
 
 export default defineConfig({
+  base: "/",
   plugins: [
     vue(),
     tailwindcss(),
